6d9b8786e36333b4ce545910bc30155dbd132d14
sashamichelle <sashaavilescalderon1999@gmail.com>
2018-01-04 10:22:55 -0300

    actualizando la caja de actualizaciones, caja de fotos y post

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,3 +57,35 @@ mensaje.appendChild(horaP);
 	cont.appendChild(mensaje);
 }
 /*aqui termina el codigo para crear post*/
+
+/*aqui comienza el codigo para agregar imagenes*/
+
+var idPin = 0;
+
+//window.onload(...)
+$(document).ready(() => {
+
+});
+
+function savePin() {
+    var titulo = $("#tituloInput").val();
+    var descripcion = $("#descripcionInput").val();
+    var url = $("#urlInput").val();
+
+    $("#pines").append("<div id='pin_" + idPin + "' class='pin col-md-3'>" +
+        //Este es el frente
+        "<div class='panel panel-success front'>" +
+        "<div class='panel-heading'>" +
+        "<h3 class='panel-title'>" + titulo + "</h3>" +
+        "</div>" +
+        "<div class = 'panel-body' >" +
+        "<a href='#' class='thumbnail'>" +
+        "<img src='" + url + "' alt='...'>" +
+        "</a>" +
+        "</div>" +
+        "<div class = 'panel-footer' >" + descripcion + "</div> </div>" +
+        //Este es el panel detrás
+        "<div class='rear'>" +
+        "</div>" +
+        "</div>");
+};
