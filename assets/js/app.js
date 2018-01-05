777aef00bab773f8ef0e8a2f6d1de4ba6ff53faa
sashamichelle <sashaavilescalderon1999@gmail.com>
2018-01-04 23:40:34 -0300

    intentando corregirlas posiciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -88,26 +88,7 @@ function savePin() {
         "<div class='rear'>" +
         "</div>" +
         "</div>");
+  };
 
-	var titulo = $("#tituloInput").val();
-	var descripcion = $("#descripcionInput").val();
-	var url = $("#urlInput").val();
 
-$("#pines").append("<div id='pin_" + idPin + "' class='pin col-md-3'>" +
-  //Este es el frente
-  "<div class='panel panel-success front'>" +
-  "<div class='panel-heading'>" +
-  "<h3 class='panel-title'>" + titulo + "</h3>" +
-  "</div>" +
-  "<div class = 'panel-body' >" +
-  "<a href='#' class='thumbnail'>" +
-  "<img src='" + url + "' alt='...'>" +
-  "</a>" +
-  "</div>" +
-  "<div class = 'panel-footer' >" + descripcion + "</div> </div>" +
-  //Este es el panel detrás
-  "<div class='rear'>" +
-  "</div>" +
-  "</div>");
 
-};
